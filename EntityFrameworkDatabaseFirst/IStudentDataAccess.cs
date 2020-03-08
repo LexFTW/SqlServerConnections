84591da5beb282dbf06c99ad66f66e0d759af01c
alexismengual <lexmengual@gmail.com>
2020-03-08 22:24:38 +0100

    Fix Bugs

---
+++ EntityFrameworkDatabaseFirst/IStudentDataAccess.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace EntityFrameworkFirstCode
+{
+    public interface IStudentDataAccess
+    {
+        bool Create(Student student);
+        Student ReadById(int id);
+        List<Student> Read(string value);
+        List<Student> Read(int value);
+        bool Update(Student student);
+        bool Delete(Student student);
+
+    }
+}
