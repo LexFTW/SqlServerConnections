142f73958ddf89565325df9b9a44ce581f4cf5d9
alexismengual <lexmengual@gmail.com>
2020-03-08 15:30:27 +0100

    Classes for CRUD

---
+++ EntityFrameworkFirstCode/IStudentDataAccess.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace EntityFrameworkFirstCode
+{
+    public interface IStudentDataAccess
+    {
+        bool Create(Student student);
+        Student ReadById(int id);
+        List<Student> Read(string query);
+        bool Update(Student student);
+        bool Delete(Student student);
+
+    }
+}
