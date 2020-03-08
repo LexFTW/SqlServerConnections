b33531c9ca8ff1fa0d549b85426b174be80387d0
soniaArenas <puente46191@gmail.com>
2020-03-08 12:56:42 +0100

    Connection Utility Class. close #10

---
+++ SqlQuerys/SqlQuerys/ConnectionUtility.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using log4net;
+using SqlQuerys.Properties;
+
+namespace SqlQuerys
+{
+    class ConnectionUtility
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(ConnectionUtility));
+        SqlConnection connectionSql;
+
+        public void OpenConnection()
+        {
+            try
+            {
+                connectionSql = new SqlConnection(GetConnectionStringByName(Resources.SqlString));
+                connectionSql.Open();
+            }
+            catch (Exception exception)
+            {
+                logger.Info("Connection Error", exception);
+            }
+        }
+
+        public void CloseConnection()
+        {
+            connectionSql.Close();
+        }
+        public string GetConnectionStringByName(string name)
+        {
+            string returnValue = null;
+            ConnectionStringSettings settings =
+                ConfigurationManager.ConnectionStrings[name];
+            if (settings != null)
+            {
+                returnValue = settings.ConnectionString;
+            }
+            logger.Info("valor Cadena " + returnValue);
+            return returnValue;
+        }
+
+    }
+}
---
+++ SqlQuerys/SqlQuerys/StudentData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlQuerys
+{
+    class StudentData : IStudentData
+    {
+        public bool Create(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Student student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
