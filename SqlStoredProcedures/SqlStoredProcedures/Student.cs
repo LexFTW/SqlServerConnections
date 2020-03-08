77bde57d0d8b3e21b5200153efbf8b82bda96839
soniaArenas <puente46191@gmail.com>
2020-03-08 19:39:35 +0100

    Student Class Project Stored Procedures. close #39

---
+++ SqlStoredProcedures/SqlStoredProcedures/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SqlStoredProcedures
+{
+    class Student
+    {
+
+        private int studentId;
+        private Guid StudentGuid;
+        private string studentName;
+        private string studentSurname;
+        private DateTime ageOfBirth;
+        private int studentAge;
+
+        public int StudentId { get => studentId; set => studentId = value; }
+
+        public string StudentName { get => studentName; set => studentName = value; }
+        public string StudentSurname { get => studentSurname; set => studentSurname = value; }
+        public DateTime AgeOfBirth { get => ageOfBirth; set => ageOfBirth = value; }
+        public int StudentAge { get => studentAge; set => studentAge = value; }
+        public Guid StudentGuid1 { get => StudentGuid; set => StudentGuid = value; }
+
+        public static List<Student> students = new List<Student>();
+
+        public Student(int Id, Guid guid, string name, string surname, DateTime dateAge, int age)
+        {
+
+            StudentId = Id;
+            StudentGuid1 = guid;
+            StudentName = name;
+            StudentSurname = surname;
+            AgeOfBirth = dateAge;
+            StudentAge = age;
+
+
+        }
+        public Student(Guid guid, string name, string surname, DateTime dateAge, int age)
+        {
+
+            StudentGuid1 = guid;
+            StudentName = name;
+            StudentSurname = surname;
+            AgeOfBirth = dateAge;
+            StudentAge = age;
+        }
+        public Student()
+        {
+
+        }
+    }
+}
