7ac7c987069fe356afe5bd52eb4d9bdf991d472b
soniaArenas <puente46191@gmail.com>
2020-03-08 12:31:24 +0100

    IStudentData Interface. close #12

---
+++ SqlQuerys/SqlQuerys/IStudentData.cs
@@ -0,0 +1,10 @@
+﻿namespace SqlQuerys
+{
+    public interface IStudentData
+    {
+        bool Create(Student student);
+        bool Read();
+        bool Update(Student student);
+        bool Delete(Student student);
+    }
+}
