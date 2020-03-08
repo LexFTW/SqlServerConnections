9ffa884566d81391387b88cc47873c1cfff20374
soniaArenas <puente46191@gmail.com>
2020-03-08 12:19:36 +0100

    Student Class. close #9

---
+++ SqlQuerys/SqlQuerys/Student.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlQuerys
+{
+    public class Student
+    {
+        private int studentId;
+        private Guid StuidetnGuid;
+        private string studentName;
+        private string studentSurname;
+        private DateTime ageOfBirth;
+        private int studentAge;
+
+        public int StudentId { get => studentId; set => studentId = value; }
+
+        public string StudentName { get => studentName; set => studentName = value; }
+        public string StudentSurname { get => studentSurname; set => studentSurname = value; }
+        public DateTime AgeOfBirth { get => ageOfBirth; set => ageOfBirth = value; }
+        public Guid StuidetnGuid1 { get => StuidetnGuid; set => StuidetnGuid = value; }
+        public int StudentAge { get => studentAge; set => studentAge = value; }
+
+
+        public static List<Student> students = new List<Student>();
+
+        public Student(int Id, Guid guid, string name, string surname, DateTime dateAge, int age)
+        {
+
+            StudentId = Id;
+            StuidetnGuid1 = guid;
+            StudentName = name;
+            StudentSurname = surname;
+            AgeOfBirth = dateAge;
+            StudentAge = age;
+
+
+        }
+        public Student(Guid guid, string name, string surname, DateTime dateAge, int age)
+        {
+
+            StuidetnGuid1 = guid;
+            StudentName = name;
+            StudentSurname = surname;
+            AgeOfBirth = dateAge;
+            StudentAge = age;
+        }
+        public Student()
+        {
+
+        }
+    }
+}
