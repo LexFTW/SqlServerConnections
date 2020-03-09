7f83bf153b2df10d75165b58128fb26cea3f2d15
alexismengual <lexmengual@gmail.com>
2020-03-09 15:18:51 +0100

    Unit Testing

---
+++ EntityFrameworkDatabaseFirstTests/StudentDataAccessUT.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace EntityFrameworkDatabaseFirst.Tests
+{
+    [TestClass()]
+    public class StudentDataAccessUT
+    {
+        private static Mock<StudentDataAccess> studentDataAcces;
+
+        [ClassInitialize()]
+        public static void TestFixtureSetup(TestContext context)
+        {
+            studentDataAcces = new Mock<StudentDataAccess>();
+        }
+
+        [TestInitialize()]
+        public void Setup()
+        {
+
+        }
+
+        [TestMethod()]
+        public void CreateUT()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteUT()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ReadUT()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ReadUT1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ReadUT2()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ReadByIdUT()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateUT()
+        {
+            Assert.Fail();
+        }
+    }
+}
